155f427e5496bb32e0acbee8a593f7197ef16c0c
craankism <saschaschwellnus@gmail.com>
2026-03-27 13:42:39 +0100

    fix: working kick command

--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,7 +1,13 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
-const data = new SlashCommandBuilder()
-	.setName('kick')
-	.setDescription('Select a member and kick them.')
-	.addUserOption((option) => option.setName('target').setDescription('The member to kick').setRequired(true))
-	.setDefaultMemberPermissions(PermissionFlagsBits.KickMembers);
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('kick')
+		.setDescription('Select a member and kick them.')
+		.addUserOption((option) => option.setName('target').setDescription('The member to kick').setRequired(true))
+		.setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+	async execute(interaction) {
+		const member = interaction.options.getMember('target');
+		member.kick();
+	},
+}
