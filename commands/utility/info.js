6ea572cf4de2c36ab88396c386418967ddc08580
craankism <saschaschwellnus@gmail.com>
2026-03-27 08:55:21 +0100

    feat: new slash commands ban and kick

--- commands/utility/info.js
+++ commands/utility/info.js
@@ -12,6 +12,17 @@ module.exports = {
         )
         .addSubcommand((subcommand) => subcommand.setName('server').setDescription('Info about the server')),
     async execute(interaction) {
-
+        if (interaction.options.getSubcommand() === 'user') {
+            const user = interaction.options.getUser('target');
+            if (user) {
+                await interaction.reply(`Username: ${user.username}\nID: ${user.id}`);
+            } else {
+                await interaction.reply(`Your username: ${interaction.user.username}\nYour ID: ${interaction.user.id}`);
+            }
+        } else if (interaction.options.getSubcommand() === 'server') {
+            await interaction.reply(
+                `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`,
+            );
+        }
     },
 };
