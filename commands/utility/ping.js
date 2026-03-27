faf5767db4951056d383d9b942c24e237064913e
craankism <saschaschwellnus@gmail.com>
2026-03-27 13:42:24 +0100

    feat: real roundtip ping

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -6,6 +6,19 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		const sent = await interaction.reply({ content: 'Pinging...', withResponse: true });
+		interaction.editReply(`Roundtrip latency: ${sent.resource.message.createdTimestamp - interaction.createdTimestamp}ms`);
+		// await interaction.reply({ content: 'Pong!', flags: MessageFlags.Ephemeral });
+		// wait interaction.followUp('Pong again!');
+		// await interaction.deferReply(); 
+		// await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+		// // you can do things that take time here (database queries, api requests, ...) that you need for the initial response
+		// // you can take up to 15 minutes, then the interaction token becomes invalid!
+		// await interaction.editReply('Pong!'); 
+
+		// await interaction.reply('Pong!');
+		// const message = await interaction.fetchReply();
+		// console.log(message);
+
 	},
 };
